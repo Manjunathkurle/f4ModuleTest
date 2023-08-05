373af3980757f8760de6fa1681889168d59a1376
Manjunathkurle <127331236+Manjunathkurle@users.noreply.github.com>
2023-08-05 22:58:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./components/Home"
+import Navbar from './components/Navbar'
+import Detail from './components/Detail'
+import "./App.css"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+       <Route path="/" element={<Home/>} />
+       <Route path="/item/:id" element={<Detail/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
